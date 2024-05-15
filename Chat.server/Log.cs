2f1ba08bebac937ee165c595a66700ec509cc78c
Vlastimil Vašut <Vasut.Vlastimil@stud.vassboskovice.cz>
2024-05-15 13:03:02 +0200

    Přidejte soubory projektu.

---
+++ Chat.server/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Chat.server
+{
+    public static class Log
+    {
+        public static void Login(string message)
+        {
+            File.AppendAllText("chatlog.txt", DateTime.Now.ToString() + "|LOGIN|" + message + "\r\n");
+        }
+    }
+}
