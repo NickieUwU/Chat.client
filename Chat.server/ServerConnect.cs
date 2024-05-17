d696ae50386d4671ef24aa5998dd2d0267c1371d
VlastimilVasut <vasutvlastimil@gmail.com>
2024-05-17 20:35:08 +0200

    Working on client

--- Chat.server/ServerConnect.cs
+++ Chat.server/ServerConnect.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Threading;
 
 namespace Chat.server
 {
-    public class ServerConnect
+    public static class ServerConnect
     {
-
+        static async Task connect(string input)
+        {
+            using(ClientWebSocket client = new ClientWebSocket())
+            {
+                Uri uri = new("\"ws://localhost:8080/ws/\"");
+                await client.ConnectAsync(uri, CancellationToken.None);
+            }
+        }
     }
 }
